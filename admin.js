1ae0a146ef14c1f3c2744e491b72c4fe1c5fc1d8
patelhet04 <hetpatel.hp4199@gmail.com>
2024-07-11 13:20:48 -0400

    Admin code setup

--- admin.js
+++ admin.js
@@ -3,5 +3,5 @@ import { Kafka } from "kafkajs";
 export const kafka = new Kafka({
   clientId: "node-kafka-demo",
   // kafka brokers - <IP>:9092
-  brokers: ["10.110.168.131:9092"],
+  brokers: ["10.0.0.10:9092"],
 });
